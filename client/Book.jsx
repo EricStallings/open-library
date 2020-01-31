17d7ddaea41bf09f1bb953edf8a919c9dd48fe29
Eric Stallings <stallings.eric@gmail.com>
2020-01-31 14:21:37 -0800

    Removed cover photo for now. Most searches caused an error to occur.

--- client/Book.jsx
+++ client/Book.jsx
@@ -5,7 +5,7 @@ const Book = (props) => {
   return(
     <li className = "book-li">
       <div className = "book-container">
-      <img className= "book-image" src={`http://covers.openlibrary.org/b/isbn/${props.bookCoverID}-M.jpg`} /> 
+      {/* <img className= "book-image" src={`http://covers.openlibrary.org/b/isbn/${props.bookCoverID}-M.jpg`} />  */}
         <p className="book-title">{props.title}</p>
         <p className ="book-subtitle">{props.subtitle}</p>
       </div>
